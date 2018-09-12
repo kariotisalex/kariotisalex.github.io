5be42171fba6c597c430368d6344cdeeb1d13d93
Alexander Kariotis <kariotis.alexander@gmail.com>
2018-09-12 18:19:38 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+/*
+setInterval(function(){
+    var interval = setInterval(function(){
+        var holder1 = Date();
+        var holder2 = holder1.split(" ")[4];
+        document.getElementById("clock").innerHTML = holder2;
+    },1000);
+})
+*/
+var choiserOfInterval = 0;
+setInterval(function(){
+
+},5000)
